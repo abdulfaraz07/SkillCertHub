fddf2290306c226104a8e0abeb4e3aadabe38028
abdulfaraz07 <141329053+abdulfaraz07@users.noreply.github.com>
2026-08-19 21:35:07 +0530

    Add Cashfree configuration for sandbox mode

--- skillcert-cashfree-config.js
+++ skillcert-cashfree-config.js
@@ -1,1 +1,10 @@
 
+window.SKILLCERT_CASHFREE = {
+  MODE: "sandbox",
+
+  CREATE_ORDER_URL:
+    "https://lmfupxuogapjgshbxcnu.supabase.co/functions/v1/cashfree-create-order",
+
+  VERIFY_ORDER_URL:
+    "https://lmfupxuogapjgshbxcnu.supabase.co/functions/v1/cashfree-verify-order"
+};
